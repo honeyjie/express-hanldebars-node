2123ca5add43ceca6b50b808aed1e3bc53743fed
honeyjie <honeyjie.xiong@gmail.com>
2017-01-20 19:13:48 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -646,8 +646,8 @@ router.post("/completeform/saveform.action", function(req, res) {
       console.log("前端请求数据", req.body, req);
      // console.log("后端返回数据", body, response)
 
-      // var data = JSON.parse(body);
-
+      var data = JSON.parse(body);
+      res.send(data);
     });
 });
 
