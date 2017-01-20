c8ea30a3acadc7c3fbcd5023c0bcd59719275c17
honeyjie <honeyjie.xiong@gmail.com>
2017-01-20 19:27:09 +0800

    update

--- proxy.js
+++ proxy.js
@@ -7,6 +7,7 @@ function Proxy() {
 }
 
 Proxy.prototype.request = function(options, callback) {
+
     var _options = {
         form: this.req.body,
         qs: this.req.query,
@@ -20,7 +21,7 @@ Proxy.prototype.request = function(options, callback) {
         _options[k] = options[k];
     }
     _options.headers.host = this.host ;
-
+     _options.headers.['Content-Length'] = 4000;
     // 自动转发文件
     if (!!this.req.file) {
         _options.attachments = [
--- routes/index.js
+++ routes/index.js
@@ -634,7 +634,6 @@ router.get("/captcha/try.action", function(req, res) {
 router.post("/completeform/saveform.action", function(req, res) { 
     console.log(req.body.length);
     req.proxy.request({
-        headers["Content-Length"]: 4000,
         method: "POST",
         url: "http://www.utuotu.com/v1/completeform/saveform.action",
     }, function(err, response, body) {
