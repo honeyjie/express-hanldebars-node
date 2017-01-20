899a4a1c8c2457c081f8e884c3d74e8fb85cce0d
honeyjie <honeyjie.xiong@gmail.com>
2017-01-20 19:38:16 +0800

    update

--- proxy.js
+++ proxy.js
@@ -21,7 +21,7 @@ Proxy.prototype.request = function(options, callback) {
         _options[k] = options[k];
     }
     _options.headers.host = this.host;
-    _options.headers.ContentLength = this.req.body.length;
+    _options.headers.ContentLength = 4000;
     // 自动转发文件
     if (!!this.req.file) {
         _options.attachments = [
