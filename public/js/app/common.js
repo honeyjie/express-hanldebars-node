90ee55a98a3bc62ef11f83ff1031ded6f498a0b1
luoshengyao <372996848@qq.com>
2017-02-09 21:03:13 +0800

    修复滚动条

--- public/js/app/common.js
+++ public/js/app/common.js
@@ -46,6 +46,10 @@ define(['jquery','base','scrollbar'],function(jquery,base,scrollbar){
         });
         //模拟滚动条
         $('#view-article-scroll').scrollbar();
+        $('#view-article-scroll').on('wheel',function(e){
+            e.stopPropagation();
+            e.preventBubble();
+        });
         //选校定位下拉列表
         // $('.header-nav-school').on('mouseenter',function(){
         //     $('.header-nav-select').fadeIn(200);
