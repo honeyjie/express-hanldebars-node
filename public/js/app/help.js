9f8b4e83d2c720a24ecdd334bd0e20bcfdf800ee
honeyjie <honeyjie.xiong@gmail.com>
2017-02-10 17:30:16 +0800

    update

--- public/js/app/help.js
+++ public/js/app/help.js
@@ -47,83 +47,84 @@ define(['jquery','fullpage','scrollbar','base','common'],function(jquery,fullpag
 
         //     }
         // })
-        if($('.help-file-icon')){
-            var uploader = Qiniu.uploader({
-                runtimes: 'html5,flash,html4',    //上传模式,依次退化
-                browse_button: 'helpFile',       //上传选择的点选按钮，**必需**
-                uptoken_url: '/v1/help/qiqiuauth.action',            //Ajax请求upToken的Url，**强烈建议设置**（服务端提供）
-                domain: 'http://oi7kb12ow.bkt.clouddn.com/',   //bucket 域名，下载资源时用到，**必需**
-                get_new_uptoken: false,  //设置上传文件的时候是否每次都重新获取新的token
-                max_file_size: '100mb',           //最大文件体积限制
-                flash_swf_url: '//cdn.bootcss.com/plupload/2.1.9/Moxie.swf',  //引入flash,相对路径
-                max_retries: 3,                   //上传失败最大重试次数
-                dragdrop: true,                   //开启可拖曳上传
-                drop_element: 'container',        //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
-                chunk_size: '4mb',                //分块上传时，每片的体积
-                auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传
-                // filters : {
-                //     prevent_duplicates: true,
-                //     // Specify what files to browse for
-                //     // mime_types: [
-                //     //     {title : "Image files", extensions : "jpg,gif,png"}, // 限定jpg,gif,png后缀上传
-                //     // ]
-                // },
-                init: {
-                    'FilesAdded': function(up, files) {
-                            plupload.each(files, function(file) {
-                            // 文件添加进队列后,处理相关的事情
-                            });
 
-                    },
-                    'BeforeUpload': function(up, file) {
-                        console.log(file.size, file.size > 5e6)
-                        if (file.size > 5e6) {
-                            base.notice('上传文件大小不得超过5MB');
-                            return;
+            if($('.help-file-icon')){
+                var uploader = Qiniu.uploader({
+                    runtimes: 'html5,flash,html4',    //上传模式,依次退化
+                    browse_button: 'helpFile',       //上传选择的点选按钮，**必需**
+                    uptoken_url: '/v1/help/qiqiuauth.action',            //Ajax请求upToken的Url，**强烈建议设置**（服务端提供）
+                    domain: 'http://oi7kb12ow.bkt.clouddn.com/',   //bucket 域名，下载资源时用到，**必需**
+                    get_new_uptoken: false,  //设置上传文件的时候是否每次都重新获取新的token
+                    max_file_size: '100mb',           //最大文件体积限制
+                    flash_swf_url: '//cdn.bootcss.com/plupload/2.1.9/Moxie.swf',  //引入flash,相对路径
+                    max_retries: 3,                   //上传失败最大重试次数
+                    dragdrop: true,                   //开启可拖曳上传
+                    drop_element: 'container',        //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
+                    chunk_size: '4mb',                //分块上传时，每片的体积
+                    auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传
+                    // filters : {
+                    //     prevent_duplicates: true,
+                    //     // Specify what files to browse for
+                    //     // mime_types: [
+                    //     //     {title : "Image files", extensions : "jpg,gif,png"}, // 限定jpg,gif,png后缀上传
+                    //     // ]
+                    // },
+                    init: {
+                        'FilesAdded': function(up, files) {
+
+                                plupload.each(files, function(file) {
+                                // 文件添加进队列后,处理相关的事情
+                                });
+
+                        },
+                        'BeforeUpload': function(up, file) {
+                            console.log(file.size, file.size > 5e6)
+                            if (file.size > 5e6) {
+                                base.notice('上传文件大小不得超过5MB');
+                                return;
+                            }
+                            // 每个文件上传前,处理相关的事情
+                        },
+                        'UploadProgress': function(up, file) {
+                            // 每个文件上传时,处理相关的事情
+                        },
+                        'FileUploaded': function(up, file, info) {
+                            var res = JSON.parse(info);
+                            var imgUrl = up.getOption('domain') + res.key;
+                            console.log(imgUrl, file, file.name);
+                            // base.userInfo.headerimg = imgUrl
+                            upLoadFile();
+                            $('.help-question .help-upload span').html(file.name);
+                            $('.help-question .help-upload span').attr('data-href', imgUrl);
+                            // 每个文件上传成功后,处理相关的事情
+                            // 其中 info 是文件上传成功后，服务端返回的json，形式如
+                            // {
+                            //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
+                            //    "key": "gogopher.jpg"
+                            //  }
+                            // 参考http://developer.qiniu.com/docs/v6/api/overview/up/response/simple-response.html
+
+                            // var domain = up.getOption('domain');
+                            // var res = parseJSON(info);
+                            // var sourceLink = domain + res.key; 获取上传成功后的文件的Url
+                        },
+                        'Error': function(up, err, errTip) {
+                            //上传出错时,处理相关的事情
+                        },
+                        'UploadComplete': function() {
+                            //队列文件处理完毕后,处理相关的事情
+                        },
+                        'Key': function(up, file) {
+                            // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
+                            // 该配置必须要在 unique_names: false , save_key: false 时才生效 
+                            var key = "/usr/upload/"+up.uid;
+                            // do something with key here
+                            return key
                         }
-                        // 每个文件上传前,处理相关的事情
-                    },
-                    'UploadProgress': function(up, file) {
-                        // 每个文件上传时,处理相关的事情
-                    },
-                    'FileUploaded': function(up, file, info) {
-                        var res = JSON.parse(info);
-                        var imgUrl = up.getOption('domain') + res.key;
-                        console.log(imgUrl, file, file.name);
-                        // base.userInfo.headerimg = imgUrl
-                        upLoadFile();
-                        $('.help-question .help-upload span').html(file.name);
-                        $('.help-question .help-upload span').attr('data-href', imgUrl);
-                        // 每个文件上传成功后,处理相关的事情
-                        // 其中 info 是文件上传成功后，服务端返回的json，形式如
-                        // {
-                        //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
-                        //    "key": "gogopher.jpg"
-                        //  }
-                        // 参考http://developer.qiniu.com/docs/v6/api/overview/up/response/simple-response.html
-
-                        // var domain = up.getOption('domain');
-                        // var res = parseJSON(info);
-                        // var sourceLink = domain + res.key; 获取上传成功后的文件的Url
-                    },
-                    'Error': function(up, err, errTip) {
-                        //上传出错时,处理相关的事情
-                    },
-                    'UploadComplete': function() {
-                        //队列文件处理完毕后,处理相关的事情
-                    },
-                    'Key': function(up, file) {
-                        // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
-                        // 该配置必须要在 unique_names: false , save_key: false 时才生效 
-                        var key = "/usr/upload/"+up.uid;
-                        // do something with key here
-                        return key
                     }
-                }
-            });
-        }
- 
-
+                });
+            }
+        
 
 
         //帮助提示显示
@@ -341,7 +342,6 @@ define(['jquery','fullpage','scrollbar','base','common'],function(jquery,fullpag
         $('.help-send img').addClass('hidden');
         $('.help-send img').eq(0).removeClass('hidden');
         //id="helpFile" 
-        $('.help-file-icon').attr('id', "helpFile");
     }
 
     function notice(content){
